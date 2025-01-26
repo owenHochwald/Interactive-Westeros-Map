1ab27f5f6d7bf6fffe37c3b891e995f327eb640f
owenh1 <owenh1@students.cs.ubc.ca>
2025-01-26 13:01:04 -0800

    add specifications for Progress class

---
+++ src/main/model/Progress.java
@@ -0,0 +1,37 @@
+package model;
+/*
+ * Represent a users progress towards visiting cities with a total number of cities and 
+ * number of cities the users has visited.
+ */
+
+public class Progress {
+
+    // EFFECTS: Creates a new Progress class with no visited cities and as many total cities 
+    //          as active cities objects
+    public Progress() {
+
+    }
+
+
+    // EFFECTS: prints the number of visited cities out of the total number of cities as a progress bar
+    public void printProgress() {
+
+    }
+
+
+    // MODIFIES: this, city
+    // EFFECTS: increases number of visited cities by 1, and mark that city as visited
+    public void visitCity(City city) {
+
+    }
+
+    // getters
+    public int getNumVisited() {
+        return 0;
+    }
+
+    public int getTotalNumCities() {
+        return 0;
+    }
+
+}
