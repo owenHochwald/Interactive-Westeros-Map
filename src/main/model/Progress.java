d4f56b6ab2529a9ed67a938a1ae167551ee4818b
owenh1 <owenh1@students.cs.ubc.ca>
2025-01-28 18:22:34 -0800

    update Progress specification to be static / shared

--- src/main/model/Progress.java
+++ src/main/model/Progress.java
@@ -1,37 +1,53 @@
 package model;
+
 /*
- * Represent a users progress towards visiting cities with a total number of cities and 
- * number of cities the users has visited.
+ * Represents a users progress towards visiting cities with a total number of cities and 
+ * number of cities the users has visited. Note, total number of cities and number of 
+ * cities visited CANNOT be negative.
  */
 
 public class Progress {
 
-    // EFFECTS: Creates a new Progress class with no visited cities and as many total cities 
-    //          as active City objects
-    public Progress() {
+    // MODIFIES: this
+    // EFFECTS: increases the total number of cities by 1
+    public static void increaseTotalCities() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: increases the number of visited cities by 1
+    public static void increaseNumVisited() {
+        // 
+    }
+
+    // MODIFIES: this
+    // EFFECTS: decrease the number of visited cities by 1
+    public static void decreaseNumVisited() {
 
     }
 
+    // MODIFIES: this
+    // EFFECTS: resets number of visited cities to zero
+    public static void resetProgress() {
 
-    // MODIFIES: this, city
-    // EFFECTS: increases number of visited cities by 1, and mark that city as visited
-    public void visitCity(City city) {
-
-    }
-
-    // setters
-    // REQUIRES: num >= totalNumCities
-    public void setTotalNumCities(int num) {
-        
     }
 
     // getters
-    public int getNumVisited() {
+    public static int getNumVisited() {
         return 0;
     }
 
-    public int getTotalNumCities() {
+    public static int getTotalNumCities() {
         return 0;
+    }
+
+    // setters
+    public static void setNumVisited(int numVisited) {
+
+    }
+
+    public static void setTotalNumCities(int getTotalNumCities) {
+        
     }
 
 }
