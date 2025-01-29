d9e63d1e1939915731cf4743dca57e651c8a854d
owenh1 <owenh1@students.cs.ubc.ca>
2025-01-28 18:32:19 -0800

    update Progress tests to use static context

--- src/test/model/TestProgress.java
+++ src/test/model/TestProgress.java
@@ -3,64 +3,104 @@ package model;
 
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TestProgress {
     Progress p;
-    City c1;
-    City c2;
 
 
     @BeforeEach
     void setup() {
-        c1 = new City("test 1", 10, "none", "Iron islands", false);
-        c2 = new City("test 2", 100, "none", "Crownlands", true);
-        p = new Progress();
+        // c1 = new City("test 1", 10, "none", "Iron islands", false);
+        // c2 = new City("test 2", 100, "none", "Crownlands", true);
+        // p = new Progress();
+        Progress.setNumVisited(0);
+        Progress.setTotalNumCities(0);
     }
 
     @Test
-    void progressConstructorTest() {
-        assertEquals(2, p.getTotalNumCities());
-        assertEquals(0, p.getNumVisited());
+    void progressInitTest() {
+        assertEquals(0, Progress.getTotalNumCities());
+        assertEquals(0, Progress.getNumVisited());
     }
 
     @Test
-    void visitCityOnceTest() {
-        assertEquals(0, p.getNumVisited());
-        p.visitCity(c1);
-        assertEquals(1, p.getNumVisited());
-        assertTrue(c1.getVisited());
+    void increaseTotalNumCitiesByOneTest() {
+        Progress.increaseTotalCities();
+        assertEquals(1, Progress.getTotalNumCities());
+    }
+
+    
+    @Test
+    void increaseTotalNumCitiesByTwoTest() {
+        Progress.increaseTotalCities();
+        Progress.increaseTotalCities();
+        assertEquals(2, Progress.getTotalNumCities());
     }
 
     @Test
-    void visitCityOnceWhenAlreadyTrueTest() {
-        c1.toggleVisited();
-        assertEquals(1, p.getNumVisited());
-        p.visitCity(c1);
-        assertEquals(1, p.getNumVisited());
-        assertTrue(c1.getVisited());
+    void increaseNumVisitedByOneTest() {
+        Progress.increaseNumVisited();
+        assertEquals(1, Progress.getNumVisited());
+    }
+
+    
+    @Test
+    void increaseNumVisitedByTwoTest() {
+        Progress.increaseNumVisited();
+        Progress.increaseNumVisited();
+        assertEquals(2, Progress.getNumVisited());
     }
 
     @Test
-    void visitTwoCitiesTest() {
-        assertEquals(0, p.getNumVisited());
-        p.visitCity(c1);
-        p.visitCity(c2);
-        assertEquals(2, p.getNumVisited());
-        assertTrue(c1.getVisited());
-        assertTrue(c2.getVisited());
+    void increaseNumVisitedByTwoAndDecreaseTest() {
+        Progress.increaseNumVisited();
+        Progress.increaseNumVisited();
+        assertEquals(2, Progress.getNumVisited());
+
+        Progress.decreaseNumVisited();
+        assertEquals(1, Progress.getNumVisited());
+
+        Progress.increaseNumVisited();
+        assertEquals(2, Progress.getNumVisited());
     }
 
     @Test
-    void setTotalNumCitiesTest() {
-        p.setTotalNumCities(5);
-        assertEquals(5, p.getTotalNumCities());
+    void decreaseNumVisitedByOneTest(){
+        Progress.increaseNumVisited();
+        assertEquals(1, Progress.getNumVisited());
 
-        p.setTotalNumCities(2);
-        assertEquals(2, p.getTotalNumCities());
+        Progress.decreaseNumVisited();
+        assertEquals(0, Progress.getNumVisited());
     }
 
+    
+    @Test
+    void decreaseNumVisitedByTwoTest(){
+        Progress.setNumVisited(3);
+        assertEquals(3, Progress.getNumVisited());
+
+        Progress.decreaseNumVisited();
+        assertEquals(2, Progress.getNumVisited());
+        Progress.decreaseNumVisited();
+        assertEquals(1, Progress.getNumVisited());
+    }
+
+    @Test
+    void resetProgressFromZeroTest() {
+        assertEquals(0, Progress.getNumVisited());
+        Progress.resetProgress();
+        assertEquals(0, Progress.getNumVisited());
+    }
+
+    @Test
+    void resetProgressFromNineTest() {
+        assertEquals(0, Progress.getNumVisited());
+        Progress.setNumVisited(9);
+        assertEquals(9, Progress.getNumVisited());
+        Progress.resetProgress();
+        assertEquals(0, Progress.getNumVisited());
+    }
 }
