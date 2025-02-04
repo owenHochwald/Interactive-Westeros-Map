9fe07749ea1c861a1e0eb88fae4acb2df5fd5d68
owenh1 <owenh1@students.cs.ubc.ca>
2025-02-04 11:23:43 -0800

    write specification for MapApp

---
+++ src/main/ui/MapApp.java
@@ -0,0 +1,78 @@
+package ui;
+
+import model.City;
+
+public class MapApp {
+
+    // EFFECTS: runs the Westeros Map App
+    public MapApp() {
+
+    }
+
+
+    // MODIFIES: this
+    // EFFECTS: handle user input
+    private void runApp() {
+
+    }
+
+    // MODIFES: this
+    // EFFECTS: inits preset locations to the map
+    private void init() {
+
+    }
+
+    // EFFECTS: displays map menu options
+    private void displayMenu() {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: processes user keyboard input
+    private void handleInput(String key) {
+
+    }
+
+    // EFFECTS: displays all locations
+    private void viewAllCities() {
+
+    }
+
+    // EFFECTS displays custom cities
+    private void viewCustomCities() {
+        
+    }
+
+
+    // MODIFIES: this
+    // EFFECTS: adds a custom city
+    private void addCity() {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: removes a custom city
+    private void removeCity() {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: marks a city as visited
+    private void markVisited() {
+
+    }
+
+    // REQUIRES: both cities are in cities
+    // MODIFIES: this
+    // EFFECTS: establishes an alliance between two cities in the map
+    private void establishAlliance(City c1, City c2) {
+
+    }
+
+    // EFFETS: shows the visited city progress as a progress bar
+    private void displayProgress() {
+
+    }
+
+
+}
