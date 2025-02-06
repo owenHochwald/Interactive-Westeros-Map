528dae62612b08680814ca2c79f01ad018f19b54
owenh1 <owenh1@students.cs.ubc.ca>
2025-02-05 22:22:29 -0800

    add specification for Location, remove entry

--- src/main/model/Location.java
+++ src/main/model/Location.java
@@ -1,5 +1,55 @@
 package model;
 
+/*
+* Location to represent map locations, like natural landmarks, with a name, region, 
+* House, and a custom made boolean
+*/
+
 public class Location {
+
+    // REQUIRES: name, region, house must not be null / empty,  population >= 10
+    // MODIFIES: this, Progress
+    // EFFECTS: creates a new unvisited location with given name, house, region,
+    //          and customMade. Increases total number of locations by 1.
+    public Location(String name, String house, String region, boolean isCapital, boolean customMade) {
+    }
+
+
+
+    // MODIFES: this
+    // EFFECTS: toggles wether a location is visited or not
+    public void toggleVisited() {
+
+    }
+
     
+    // setters
+    public void setHouse(String house) {
+
+    }
+
+    // getters
+
+    public String getName() {
+        return "";
+    }
+
+    public String getRegion() {
+        return "";
+    }
+
+    public String getHouse() {
+        return "";
+    }
+
+    public boolean getVisited() {
+        return false;
+    }
+
+    public boolean customMade() {
+        return false;
+    }
 }
+    
+
+
