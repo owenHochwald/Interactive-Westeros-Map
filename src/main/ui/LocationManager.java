adac74370a788786aa902527b03cf89f76b52d0b
owenHochwald <owenhochwald@gmail.com>
2025-02-14 23:17:13 -0500

    move location management into seperate class

---
+++ src/main/ui/LocationManager.java
@@ -0,0 +1,100 @@
+package ui;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import model.Location;
+import model.Progress;
+
+public class LocationManager {
+    private ArrayList<Location> locations;
+    private Scanner input;
+
+    public LocationManager(ArrayList<Location> locations, Scanner input) {
+        this.locations = locations;
+        this.input = input;
+    }
+
+    // MODIFES: this, Progress
+    // EFFECTS: displays location management options
+    // and logic to direct user to next step
+    public void manageLocations() {
+        System.out.println("╔════════════════════════════════════════╗");
+        System.out.println("║       MANAGE Locations                 ║");
+        System.out.println("╠════════════════════════════════════════╣");
+        System.out.println("║  [1] Add a location                    ║");
+        System.out.println("║  [2] Remove a location                 ║");
+        System.out.println("║  [4] Go back                           ║");
+        System.out.println("╠════════════════════════════════════════╣");
+        System.out.print("║  Enter your choice: ");
+
+        String command = input.next();
+        handleMangeLocationInput(command);
+    }
+
+    // MODIFIES: this
+    // EFFECTS: processes user keyboard input for custom location view
+    private void handleMangeLocationInput(String key) {
+        switch (key) {
+            case "1":
+                addLocation();
+                break;
+            case "2":
+                removeLocation();
+                break;
+            case "3":
+                break;
+            default:
+                System.out.println("Invalid selection!");
+        }
+    }
+
+    // MODIFIES: this
+    // EFFECTS: adds a custom location
+    private void addLocation() {
+        System.out.println("\nEnter location details:");
+
+        System.out.print("Name: ");
+        String name = input.next();
+        input.nextLine();
+
+        System.out.print("Region: ");
+        String region = input.nextLine();
+
+        Location newLocation = new Location(name, region, true);
+        locations.add(newLocation);
+
+        System.out.println("Added " + name + " successfully!");
+    }
+
+    // MODIFIES: this, Progress
+    // EFFECTS: removes a location if its a custom location, nothing if its not
+    private void removeLocation() {
+        System.out.println("╔════════════════════════════════════════════════════════════╗");
+        System.out.println("║ Enter a location name to remove: ");
+        String locationName = input.next();
+        Location locationToRemove = findLocationByName(locationName);
+
+        if (locationToRemove.customMade()) {
+            if (locationToRemove.getVisited()) {
+                Progress.decreasesNumVisitedEntries();
+            }
+            Progress.decreasesNumEntries();
+            locations.remove(locationToRemove);
+        } else {
+            System.out.println("We can't remove a original locations, my lord!");
+        }
+    }
+
+    // REQUIRES: name in the locations names
+    // EFFECTS: returns the locations with the matching name, nothing if not found
+    private Location findLocationByName(String name) {
+        for (Location location : locations) {
+            if (location.getName().equals(name)) {
+                return location;
+            }
+        }
+        return null;
+    }
+
+}
--- src/main/ui/MapApp.java
+++ src/main/ui/MapApp.java
@@ -23,6 +23,8 @@ public class MapApp {
     private Scanner input;
     private JsonReader jsonReader;
     private JsonWriter jsonWriter;
+    private LocationManager locationManager;
+
 
     // EFFECTS: runs the Westeros Map App
     public MapApp() {
@@ -61,6 +63,7 @@ public class MapApp {
         this.input = new Scanner(System.in);
         initCities();
         initLocations();
+        locationManager = new LocationManager(locations, input);
     }
 
     // MODIFIES: this
@@ -128,7 +131,7 @@ public class MapApp {
 
         String command = input.next().toLowerCase();
         if (command.equals("1")) {
-            manageLocations();
+            locationManager.manageLocations();
         } else if (command.equals("2")) {
             manageCities();
         } else {
@@ -175,39 +178,6 @@ public class MapApp {
         }
     }
 
-    // MODIFES: this, Progress
-    // EFFECTS: displays location management options
-    // and logic to direct user to next step
-    private void manageLocations() {
-        System.out.println("╔════════════════════════════════════════╗");
-        System.out.println("║       MANAGE Locations                 ║");
-        System.out.println("╠════════════════════════════════════════╣");
-        System.out.println("║  [1] Add a location                    ║");
-        System.out.println("║  [2] Remove a location                 ║");
-        System.out.println("║  [4] Go back                           ║");
-        System.out.println("╠════════════════════════════════════════╣");
-        System.out.print("║  Enter your choice: ");
-
-        String command = input.next();
-        handleMangeLocationInput(command);
-    }
-
-    // MODIFIES: this
-    // EFFECTS: processes user keyboard input for custom location view
-    private void handleMangeLocationInput(String key) {
-        switch (key) {
-            case "1":
-                addLocation();
-                break;
-            case "2":
-                removeLocation();
-                break;
-            case "3":
-                break;
-            default:
-                System.out.println("Invalid selection!");
-        }
-    }
 
     // EFFECTS: displays all cities one by one
     private void viewAllCities() {
@@ -374,23 +344,6 @@ public class MapApp {
         System.out.println("╚════════════════════════════════════════════════════════════╝");
     }
 
-    // MODIFIES: this
-    // EFFECTS: adds a custom location
-    private void addLocation() {
-        System.out.println("\nEnter location details:");
-
-        System.out.print("Name: ");
-        String name = input.next();
-        input.nextLine();
-
-        System.out.print("Region: ");
-        String region = input.nextLine();
-
-        Location newLocation = new Location(name, region, true);
-        locations.add(newLocation);
-
-        System.out.println("Added " + name + " successfully!");
-    }
 
     // MODIFIES: this
     // EFFECTS: adds a custom city
@@ -467,24 +420,24 @@ public class MapApp {
         }
     }
 
-    // MODIFIES: this, Progress
-    // EFFECTS: removes a location if its a custom location, nothing if its not
-    private void removeLocation() {
-        System.out.println("╔════════════════════════════════════════════════════════════╗");
-        System.out.println("║ Enter a location name to remove: ");
-        String locationName = input.next();
-        Location locationToRemove = findLocationByName(locationName);
+    // // MODIFIES: this, Progress
+    // // EFFECTS: removes a location if its a custom location, nothing if its not
+    // private void removeLocation() {
+    //     System.out.println("╔════════════════════════════════════════════════════════════╗");
+    //     System.out.println("║ Enter a location name to remove: ");
+    //     String locationName = input.next();
+    //     Location locationToRemove = findLocationByName(locationName);
 
-        if (locationToRemove.customMade()) {
-            if (locationToRemove.getVisited()) {
-                Progress.decreasesNumVisitedEntries();
-            }
-            Progress.decreasesNumEntries();
-            locations.remove(locationToRemove);
-        } else {
-            System.out.println("We can't remove a original locations, my lord!");
-        }
-    }
+    //     if (locationToRemove.customMade()) {
+    //         if (locationToRemove.getVisited()) {
+    //             Progress.decreasesNumVisitedEntries();
+    //         }
+    //         Progress.decreasesNumEntries();
+    //         locations.remove(locationToRemove);
+    //     } else {
+    //         System.out.println("We can't remove a original locations, my lord!");
+    //     }
+    // }
 
     // MODIFIES: this
     // EFFECTS: establishes an alliance between two cities in the map
@@ -509,17 +462,6 @@ public class MapApp {
         for (City city : cities) {
             if (city.getName().equals(name)) {
                 return city;
-            }
-        }
-        return null;
-    }
-
-    // REQUIRES: name in the locations names
-    // EFFECTS: returns the locations with the matching name, nothing if not found
-    private Location findLocationByName(String name) {
-        for (Location location : locations) {
-            if (location.getName().equals(name)) {
-                return location;
             }
         }
         return null;
