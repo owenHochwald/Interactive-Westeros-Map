feebac33ba93406a1e05c8849b6ef5ff3fa817c1
owenHochwald <owenhochwald@gmail.com>
2025-02-17 12:37:39 -0500

    add specification for menu bar

---
+++ src/main/ui/gui/MenuBar.java
@@ -0,0 +1,50 @@
+package ui.gui;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JMenuBar;
+import javax.swing.JProgressBar;
+
+/*
+ * Represents a menu bar that will be displayed at the top of the page.
+ * Has the ability to display the users progress in visited locations,
+ * manage entries and view entries.
+ */
+public class MenuBar extends JMenuBar {
+
+    // EFFECTS: creates a menu bar with title, progress, and actions
+    public MenuBar() {
+
+    }
+
+
+    // getters
+    public JButton getSaveButton() {
+        return null;
+    }
+
+    public JButton getLoadButton() {
+        return null;
+    }
+
+    public JButton getViewCitiesButton() {
+        return null;
+    }
+
+    public JButton getViewLocationsButton() {
+        return null;
+    }
+
+    public JButton getAddEntryButton() {
+        return null;
+    }
+
+    public JProgressBar getProgressBar() {
+        return null;
+    }
+
+    public JLabel getTitle() {
+        return null;
+    }
+
+}
