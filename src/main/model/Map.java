706398baa6c03a1ed65c593006221fb2fcd94475
owenHochwald <owenhochwald@gmail.com>
2025-02-17 13:15:13 -0500

    add specification for Map class

---
+++ src/main/model/Map.java
@@ -0,0 +1,46 @@
+package model;
+
+import java.util.ArrayList;
+
+/*
+ * Represents a Map with locations and cities. Provides methods on that data 
+ * to interact and manage the map.
+ */
+public class Map {
+
+    // EFFECTS: constructs a new map with cities and locations
+    public Map() {
+
+    }
+
+
+    // MODIFIES: this
+    // EFFECTS: loads the map state from file
+    public void loadMap() {
+
+    }
+
+    // MODIFIES: mapState
+    // EFFECTS: saves the current map state to file
+    public void saveMap() {
+
+    }
+
+    // getters
+    public ArrayList<Location> getLocations() {
+        return null;
+    }
+
+    public ArrayList<City> getCities() {
+        return null;
+    }
+
+    public void setLocations(ArrayList<Location> locations) {
+
+    }
+
+    public void setCities(ArrayList<City> cities) {
+        
+    }
+
+}
--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -2,12 +2,10 @@ package ui;
 
 import ui.cli.MapApp;
 import ui.gui.MapGUI;
-import ui.gui.MenuBar;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // new MapApp();
-        new MapGUI();
-        // new MenuBar();
+        new MapApp();
+        // new MapGUI();
     }
 }
