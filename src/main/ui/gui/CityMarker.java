36bb3e17624a93b0987d1836d2892b373c806052
owenHochwald <owenhochwald@gmail.com>
2025-02-18 11:35:00 -0500

    add specification for CityMarker

---
+++ src/main/ui/gui/CityMarker.java
@@ -0,0 +1,47 @@
+package ui.gui;
+
+/*
+ * Represents a marker for a city to be displayed on the MapPanel
+ */
+public class CityMarker {
+
+    // EFFECTS: constructs a marker with an x and y position, diameter, and a city
+    // name
+    public CityMarker(int x, int y, int diameter, String name) {
+
+    }
+
+    // setters
+    public void setX() {
+
+    }
+
+    public void setY() {
+
+    }
+
+    public void setDiameter() {
+
+    }
+
+    public void setName() {
+
+    }
+
+    // getters
+    public int getX() {
+        return 0;
+    }
+
+    public int getY() {
+        return 0;
+    }
+
+    public int getDiameter() {
+        return 0;
+    }
+
+    public String getName() {
+        return null;
+    }
+}
