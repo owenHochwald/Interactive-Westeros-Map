0cf448fb326a87eb748a3fc00b6ae86aa325c67f
owenHochwald <owenhochwald@gmail.com>
2025-02-18 09:31:39 -0500

    add tests for JsonReaderTest readMap

--- src/test/persistence/JsonReaderTest.java
+++ src/test/persistence/JsonReaderTest.java
@@ -10,17 +10,30 @@ import org.junit.Test;
 
 import model.City;
 import model.Location;
+import model.Map;
 
 public class JsonReaderTest extends JsonTest {
 
     private ArrayList<Location> locations;
     private ArrayList<City> cities;
+    private Map map;
 
     @Test
     public void testReaderNonExistentFileForLocation() {
         JsonReader reader = new JsonReader("./data/noSuchFile.json");
         try {
             locations = reader.readLocations();
+            fail("IOException expected");
+        } catch (IOException e) {
+            // pass
+        }
+    }
+
+    @Test
+    public void testMapReaderWithNoFilePath() {
+        JsonReader reader = new JsonReader("./data/noSuchFile.json");
+        try {
+            map = reader.readMap();
             fail("IOException expected");
         } catch (IOException e) {
             // pass
@@ -70,6 +83,29 @@ public class JsonReaderTest extends JsonTest {
             checkLocation(locations.get(0), "Kings Road", "Crownlands", true, true);
             checkLocation(locations.get(1), "The Gods Eye", "The Riverlands", false, false);
             assertEquals("Winterfell", cities.get(1).getAlliances().get(0));
+        } catch (IOException e) {
+            fail("Couldn't read from file");
+        }
+    }
+
+    @Test
+    public void testReaderGeneralWorkRoomWithMap() {
+        JsonReader reader = new JsonReader("./data/testReaderGeneralMapState.json");
+        try {
+            map = reader.readMap();
+            assertEquals(2, map.getCities().size());
+            assertEquals(2, map.getLocations().size());
+            ArrayList<String> a1 = new ArrayList<>();
+            ArrayList<String> a2 = new ArrayList<>();
+            a1.add("Kings Landing");
+            a2.add("Winterfell");
+            checkCity(map.getCities().get(0), "Winterfell", 15000, "Stark", "The North", true,
+                    false, true, a1);
+            checkCity(map.getCities().get(1), "Kings Landing", 1000000, "Lannister", "Crownlands", true,
+                    true, false, a2);
+            checkLocation(map.getLocations().get(0), "Kings Road", "Crownlands", true, true);
+            checkLocation(map.getLocations().get(1), "The Gods Eye", "The Riverlands", false, false);
+            assertEquals("Winterfell", map.getCities().get(1).getAlliances().get(0));
         } catch (IOException e) {
             fail("Couldn't read from file");
         }
