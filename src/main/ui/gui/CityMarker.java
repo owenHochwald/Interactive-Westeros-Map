7a61701582fcfac0dc067ef7e249b8a88199d0de
owenHochwald <owenhochwald@gmail.com>
2025-02-18 11:36:47 -0500

    implement CityMarker

--- src/main/ui/gui/CityMarker.java
+++ src/main/ui/gui/CityMarker.java
@@ -4,44 +4,51 @@ package ui.gui;
  * Represents a marker for a city to be displayed on the MapPanel
  */
 public class CityMarker {
+    private int x;
+    private int y;
+    private int diameter;
+    private String name;
 
     // EFFECTS: constructs a marker with an x and y position, diameter, and a city
     // name
     public CityMarker(int x, int y, int diameter, String name) {
-
+        this.x = x;
+        this.y= y;
+        this.diameter = diameter;
+        this.name = name;
     }
 
     // setters
-    public void setX() {
-
+    public void setX(int x) {
+        this.x = x;
     }
 
-    public void setY() {
-
+    public void setY(int y) {
+        this.y = y;
     }
 
-    public void setDiameter() {
-
+    public void setDiameter(int diameter) {
+        this.diameter = diameter;
     }
 
-    public void setName() {
-
+    public void setName(String name) {
+        this.name = name;
     }
 
     // getters
     public int getX() {
-        return 0;
+        return x;
     }
 
     public int getY() {
-        return 0;
+        return y;
     }
 
     public int getDiameter() {
-        return 0;
+        return diameter;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 }
